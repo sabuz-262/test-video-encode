35336411069400d5e3ffd709da666724acc7975e
charles.s.anderson <charles.s.anderson@gmail.com>
2011-12-27 16:28:36 +0000

    This now encodes a single frame.  Next version should include the ability to encode the live data.

--- TestVideoEncode/src/com/csa/video/TestVideoBase.java
+++ TestVideoEncode/src/com/csa/video/TestVideoBase.java
@@ -2,15 +2,12 @@ package com.csa.video;
 
 import com.googlecode.javacpp.BytePointer;
 import com.googlecode.javacpp.Pointer;
-import com.googlecode.javacv.FFmpegFrameRecorder;
 import com.googlecode.javacv.cpp.avcodec;
 import com.googlecode.javacv.cpp.avcodec.AVCodec;
 import com.googlecode.javacv.cpp.avcodec.AVCodecContext;
 import com.googlecode.javacv.cpp.avcodec.AVFrame;
 import com.googlecode.javacv.cpp.avcodec.AVPacket;
-import com.googlecode.javacv.cpp.avcodec.AVPicture;
 import com.googlecode.javacv.cpp.avformat;
-import com.googlecode.javacv.cpp.avformat.AVFormatContext;
 import com.googlecode.javacv.cpp.avformat.AVOutputFormat;
 import com.googlecode.javacv.cpp.avformat.AVStream;
 import com.googlecode.javacv.cpp.avutil;
@@ -18,9 +15,7 @@ import com.googlecode.javacv.cpp.avutil.AVRational;
 import com.googlecode.javacv.cpp.swscale.SwsContext;
 
 import android.app.Activity;
-import android.graphics.YuvImage;
 import android.hardware.Camera;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -58,10 +53,6 @@ public class TestVideoBase extends Activity implements Camera.PreviewCallback
 	boolean firstTime = true;
 	
     private AVFrame picture;
-    private Pointer video_outbuf;
-    private int video_outbuf_size;
-    //private AVOutputFormat fmt;
-    private AVFormatContext oc;
     private AVCodecContext c;
     private AVFrame coded_frame;
     private AVOutputFormat oformat;
@@ -71,111 +62,110 @@ public class TestVideoBase extends Activity implements Camera.PreviewCallback
     //private PointerByReference p = new PointerByReference();
     public static final int DEFAULT_FRAME_RATE_BASE = 1001000;
 
+    int i, out_size, size, x, y, output_buffer_size;
+	//FILE            *file;
+    Pointer output_buffer;
+    Pointer picture_buffer;
+    
 	public TestVideoBase() throws Exception
 	{
+		avcodec.avcodec_register_all();
+		avcodec.avcodec_init();
 		avformat.av_register_all();
-		
-//		fmt = avformat.av_guess_format(null, "h.3gp", null);
-//		if (fmt == null) throw new Exception("Couldn't find format");
 
-		oc = avformat.avformat_alloc_context();
-		if (oc == null) throw new Exception("Couldn't alloc context.");
-		//oc.oformat(fmt);
-		
-		video_st = avformat.av_new_stream(oc, 0);
-		if (video_st == null) throw new Exception("Couldn't alloc stream.");
+		/* Manual Variables */
+		int             l;
+		int             fps = 30;
+		int             videoLength = 5;
 
-		c = new AVCodecContext(video_st.codec());
-		if (c == null) throw new Exception("Couldn't get context.");
-		c.codec_id(avcodec.CODEC_ID_H263);
-		c.codec_type(avcodec.CODEC_TYPE_VIDEO);
-		c.bit_rate(400000);
-		c.width(352);
-		c.height(288);
-		c.time_base(avutil.av_d2q(1/30, DEFAULT_FRAME_RATE_BASE));
-		c.gop_size(12);
-		c.pix_fmt(avutil.PIX_FMT_YUV420P);
-		c.flags(c.flags() | avcodec.CODEC_FLAG_GLOBAL_HEADER);
-		
-		if (avformat.av_set_parameters(oc, null) < 0) throw new Exception("Invalid output format parameters");
-		
-		AVCodec codec = avcodec.avcodec_find_encoder(c.codec_id());
-		if (codec == null) throw new Exception("Codec not found!");
-		if (avcodec.avcodec_open(c, codec) < 0) throw new Exception("Could not open codec.");
-		
-		coded_frame = new AVFrame(c.coded_frame());
-		
+		/* find the H263 video encoder */
+		mCodec = avcodec.avcodec_find_encoder(avcodec.CODEC_ID_H263);
+		if (mCodec == null) {
+		    Log.d("TEST_VIDEO", "avcodec_find_encoder() run fail.");
+		}
+
+		mCodecCtx = avcodec.avcodec_alloc_context();
 		picture = avcodec.avcodec_alloc_frame();
-		if (picture == null)
-		{
-			avcodec.avcodec_close(c);
-			avutil.av_freep(video_st);
-			video_st = null;
-			throw new Exception("Could not allocate picture");
+
+		/* put sample parameters */
+		mCodecCtx.bit_rate(400000);
+		/* resolution must be a multiple of two */
+		mCodecCtx.width(VIDEO_WIDTH);
+		mCodecCtx.height(VIDEO_HEIGHT);
+		/* frames per second */
+		AVRational avFPS = new AVRational();
+		avFPS.num(1);
+		avFPS.den(VIDEO_FPS);
+		mCodecCtx.time_base(avFPS);
+		mCodecCtx.pix_fmt(avutil.PIX_FMT_YUV420P);
+		mCodecCtx.codec_id(avcodec.CODEC_ID_H263);
+		mCodecCtx.codec_type(avutil.AVMEDIA_TYPE_VIDEO);
+
+		/* open it */
+		if (avcodec.avcodec_open(mCodecCtx, mCodec) < 0) {
+		    Log.d("TEST_VIDEO", "avcodec_open() run fail.");
 		}
-		int size = avcodec.avpicture_get_size(c.pix_fmt(), c.width(), c.height());
-		Pointer picture_buf = avutil.av_malloc(size);
-		if (picture_buf == null)
-		{
-			avutil.av_free(picture);
-			picture = null;
+
+//		const char* mfileName = (*env)->GetStringUTFChars(env, filename, 0);
+//
+//		file = fopen(mfileName, "wb");
+//		if (!file) {
+//		    LOGI("fopen() run fail.");
+//		}
+//
+//		(*env)->ReleaseStringUTFChars(env, filename, mfileName);
+//
+		/* alloc image and output buffer */
+		output_buffer_size = 100000;
+		output_buffer = avutil.av_malloc(output_buffer_size);
+
+		size = mCodecCtx.width() * mCodecCtx.height();
+		picture_buffer = avutil.av_malloc((size * 3) / 2); /* size for YUV 420 */
+
+		picture.data(0, new BytePointer(picture_buffer));
+		picture.data(1, picture.data(0).position(size));
+		picture.data(2, picture.data(1).position(size / 4));
+		picture.linesize(0, mCodecCtx.width());
+		picture.linesize(1, mCodecCtx.width() / 2);
+		picture.linesize(2, mCodecCtx.width() / 2);
+
+		for(l=0;l<videoLength;l++){
+		    //encode 1 second of video
+		    for(i=0;i<fps;i++) {
+		        //prepare a dummy image YCbCr
+		        //Y
+		        for(y=0;y<mCodecCtx.height();y++) {
+		            for(x=0;x<mCodecCtx.width();x++) {
+		                picture.data(0).put((y * picture.linesize(0) + x), (byte)(x + y + i * 3));
+		            }
+		        }
+
+		        //Cb and Cr
+		        for(y=0;y<mCodecCtx.height()/2;y++) {
+		            for(x=0;x<mCodecCtx.width()/2;x++) {
+		                picture.data(1).put((y * picture.linesize(1) + x), (byte)(128 + y + i * 2));
+		                picture.data(2).put((y * picture.linesize(2) + x), (byte)(64 + x + i * 5));
+		            }
+		        }
+
+		        //encode the image
+		        out_size = avcodec.avcodec_encode_video(mCodecCtx, new BytePointer(output_buffer), output_buffer_size, picture);
+		        Log.d("TEST_VIDEO", "Encoded '" + out_size + "' bytes");
+		        //fwrite(output_buffer, 1, out_size, file);
+		    }
+
+		    //get the delayed frames
+		    for(; out_size > 0; i++) {
+		        out_size = avcodec.avcodec_encode_video(mCodecCtx, new BytePointer(output_buffer), output_buffer_size, null);
+		        Log.d("TEST_VIDEO", "Encoded '" + out_size + "' bytes");
+		        //fwrite(output_buffer, 1, out_size, file);
+		    }
 		}
-		else
-		{
-			avcodec.avpicture_fill((AVPicture)picture, (BytePointer)picture_buf, c.pix_fmt(), c.width(), c.height());
-		}
-		avformat.av_write_header(oc);
-		
-		
+		avcodec.avcodec_close(mCodecCtx);
+		avutil.av_free(mCodecCtx);
+		avutil.av_free(picture);
+
 		return;
-//		//Instantiate encoders/decoders, set up screen for surface
-//		avcodec.avcodec_register_all();
-//		avcodec.avcodec_init();
-//		avformat.av_register_all();
-//		//avformat.av_set_parameters(mCodecCtx, null);
-//		
-//		mCodec = avcodec.avcodec_find_encoder(avcodec.CODEC_ID_H263);
-//		if (mCodec == null) return;
-//		
-//		mCodecCtx = avcodec.avcodec_alloc_context();
-//		mCodecCtx.bit_rate(300000);
-//		mCodecCtx.codec(mCodec);
-//		mCodecCtx.width(VIDEO_WIDTH);
-//		mCodecCtx.height(VIDEO_HEIGHT);
-//		mCodecCtx.pix_fmt(avutil.PIX_FMT_YUV420P);
-//		mCodecCtx.codec_id(avcodec.CODEC_ID_H263);
-//		mCodecCtx.codec_type(avcodec.CODEC_TYPE_VIDEO);
-//
-//		mRatio = new AVRational();
-//		mRatio.num(1);
-//		mRatio.den(VIDEO_FPS);
-//		mCodecCtx.time_base(mRatio);
-//		mCodecCtx.coder_type(1);
-//		mCodecCtx.flags(mCodecCtx.flags() | avcodec.CODEC_FLAG_LOOP_FILTER);
-//		mCodecCtx.me_cmp(avcodec.FF_LOSS_CHROMA);
-//		mCodecCtx.me_method(avcodec.ME_HEX);
-//		mCodecCtx.me_subpel_quality(6);
-//		mCodecCtx.me_range(16);
-//		mCodecCtx.gop_size(30);
-//		mCodecCtx.keyint_min(10);
-//		mCodecCtx.scenechange_threshold(40);
-//		mCodecCtx.i_quant_factor((float) 0.71);
-//		mCodecCtx.b_frame_strategy(1);
-//		mCodecCtx.qcompress((float) 0.6);
-//		mCodecCtx.qmin(10);
-//		mCodecCtx.qmax(51);
-//		mCodecCtx.max_qdiff(4);
-//		mCodecCtx.max_b_frames(1);
-//		mCodecCtx.refs(2);
-//		mCodecCtx.directpred(3);
-//		mCodecCtx.trellis(1);
-//		mCodecCtx.flags2(mCodecCtx.flags2() | avcodec.CODEC_FLAG2_BPYRAMID | avcodec.CODEC_FLAG2_WPRED | avcodec.CODEC_FLAG2_8X8DCT | avcodec.CODEC_FLAG2_FASTPSKIP);
-//
-//		if (avcodec.avcodec_open(mCodecCtx, mCodec) == 0) return;
-//
-//		mFrameSize = avcodec.avpicture_get_size(avutil.PIX_FMT_YUV420P, VIDEO_WIDTH, VIDEO_HEIGHT);
-//		mFrame = avcodec.avcodec_alloc_frame();
-//		mPacket = new AVPacket();
 	}
 
     /** Called when the activity is first created. */
@@ -195,86 +185,7 @@ public class TestVideoBase extends Activity implements Camera.PreviewCallback
 	@Override
 	public void onPreviewFrame(byte[] data, Camera camera)
 	{
-		if (!firstTime) return;
-		firstTime = false;
-
-		if (data == null)
-		{
-			Log.d("TEST_VIDEO", "No data");
-			return;
-		}
-		
-		Camera.Parameters params = camera.getParameters();
-		Size previewSize = params.getPreviewSize();
-		byte[] data420 = new byte[data.length];
-		convert_yuv422_to_yuv420(data, data420, previewSize.width, previewSize.height);
-		
-		if (video_st == null)
-		{
-			try {
-				throw new Exception ("No video output stream.");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		if (picture == null)
-		{
-			Log.d("TEST_VIDEO", "No picture.");
-			return;
-		}
-		
-		picture.data(0, new BytePointer(data420));
-		picture.linesize(0, previewSize.width * 3);
-		
-		int out_size = avcodec.avcodec_encode_video(c, (BytePointer) video_outbuf, video_outbuf_size, picture);
-		if (out_size > 0)
-		{
-			Log.d("TEST_VIDEO", "out_size = '" + out_size + "'");
-			avcodec.av_init_packet(pkt);
-			if (coded_frame.pts() != avcodec.AV_NOPTS_VALUE)
-			{
-				pkt.pts(avutil.av_rescale_q(coded_frame.pts(), c.time_base(), video_st.time_base()));
-			}
-			if (coded_frame.key_frame() != 0)
-			{
-				pkt.flags(pkt.flags() | avcodec.PKT_FLAG_KEY);
-			}
-			pkt.stream_index(video_st.index());
-			pkt.data((BytePointer) video_outbuf);
-			pkt.size(out_size);
-			Log.d("TEST_VIDEO", "write: " + avformat.av_write_frame(oc, pkt));
-		}
-		Log.d("TEST_VIDEO", "no out_size");
-//		int bOutBuffSize = VIDEO_WIDTH * VIDEO_HEIGHT * 4;
-//		BytePointer picPointer = new BytePointer(data);
-//		BytePointer bBuffer = new BytePointer(avutil.av_malloc(bOutBuffSize));
-//
-//		if (avcodec.avpicture_fill((AVPicture)mFrame, picPointer, avutil.PIX_FMT_YUV420P, VIDEO_WIDTH, VIDEO_HEIGHT) <= 0)
-//			return;
-//
-//		//encode the image
-//		int size = avcodec.avcodec_encode_video(mCodecCtx, bBuffer, bOutBuffSize, mFrame);
-//		
-//		//Image was buffered
-//		if (size == 0)
-//			return;
-//		
-//		if (size > 0)
-//		{
-//			avcodec.av_init_packet(mPacket);
-//			AVFrame coded_frame = mCodecCtx.coded_frame();
-//			long pts = coded_frame.pts();
-//			if (coded_frame.pts() != avcodec.AV_NOPTS_VALUE)
-//				mPacket.pts(avutil.av_rescale_q(pts, mCodecCtx.time_base(), mCodecCtx.time_base()));
-//			if (coded_frame.key_frame() != 0)
-//				mPacket.flags(mPacket.flags() | avcodec.PKT_FLAG_KEY);
-//			mPacket.stream_index(mPacket.stream_index() + 1);
-//			mPacket.data(bBuffer);
-//			mPacket.size(size);
-//		}
-//		Log.d("TEST_VIDEO", "Size encoded: '" + size + "'.");
+		//convert byte[] first, then encode, then packetize
 	}
 
 	void convert_yuv422_to_yuv420(byte[] inBuff, byte[] outBuff, int width, int height)
